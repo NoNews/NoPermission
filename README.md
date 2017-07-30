96edff4e366bcea260dc6a77733b11610c91dbe4
NoNews <me@alexbykov.ru>
2017-07-30 19:27:05 +0300

    initial

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# NoPermossion
+# NoPermission
 Android library for permissions request
+
+
+### Gradle
+
+    compile 'ru.alexbykov:nopermission:1.0.0'
+
+
+### Install
+
+```java
+PermissionHelper permissionHelper = new PermissionHelper(context);
+
+permissionHelper.check(Manifest.permission.READ_CONTACTS)
+                .onSuccess(this::onSuccess)
+                .onFailure(this::onFailure)
+                .run();
+```
+
+Multiply permissions:
+
+```java
+
+   permissionHelper.check(Manifest.permission.READ_CONTACTS, Manifest.permission.READ_CONTACTS, Manifest.permission.READ_PHONE_STATE)
+                   .onSuccess(this::onSuccess)
+                   .onFailure(this::onFailure)
+                   .run();
+```
+
+Don't forget onActivityResult:
+
+```java
+ @Override
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+
+        permissionHelper.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+   }
+```
---
+++ library/src/main/java/ru/alexbykov/nopermission/PermissionHelper.java
@@ -0,0 +1,75 @@
+package ru.alexbykov.nopermission;
+
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+
+/**
+ * Date: 30.07.2017
+ * Time: 18:39
+ * Project: NoPermission
+ *
+ * @author Mike Antipiev @nindzyago
+ * @author Alex Bykov @NoNews
+ */
+public class PermissionHelper {
+
+    private static final int PERMISSION_REQUEST_CODE = 1005001;
+    private Activity activity;
+    private String[] permissions;
+    private OnPermissionSuccessListener successListener;
+    private OnPermissionFailureListener failureListener;
+
+    public PermissionHelper(Activity activity) {
+        this.activity = activity;
+    }
+
+    public PermissionHelper check(String permission) {
+        this.permissions[0] = permission;
+        return this;
+    }
+
+    public PermissionHelper check(String... permissions) {
+        this.permissions = permissions;
+        return this;
+    }
+
+    public PermissionHelper onSuccess(OnPermissionSuccessListener listener) {
+        this.successListener = listener;
+        return this;
+    }
+
+    public PermissionHelper onFailure(OnPermissionFailureListener listener) {
+        this.failureListener = listener;
+        return this;
+    }
+
+    public void run() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            activity.requestPermissions(permissions, PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED) {
+                    if (failureListener != null)
+                        failureListener.onFailure();
+                    return;
+                }
+            }
+            if (successListener != null)
+                successListener.onSuccess();
+        }
+    }
+
+    public interface OnPermissionSuccessListener {
+        void onSuccess();
+    }
+
+    public interface OnPermissionFailureListener {
+        void onFailure();
+    }
+}
